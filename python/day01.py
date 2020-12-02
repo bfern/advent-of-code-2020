90471105b1c8a2ce151447732f7c02024e42962b
Brad <bfern94@yahoo.com>
2020-12-02 21:03:48 +0000

    fix_expense_report function

--- python/day01.py
+++ python/day01.py
@@ -1,2 +1,8 @@
+import numpy as np
+
 def fix_expense_report(report: list) -> int:
+    report_subtract_2020 = 2020 - np.array(report)
+    for val in report_subtract_2020:
+        if val in report:
+            return val * (2020 - val)
     return 0
