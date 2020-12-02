bac6337cda805dd43860ed89dd7b444565255258
Brad <bfern94@yahoo.com>
2020-12-02 21:36:24 +0000

    fix expense report part two

--- python/day01.py
+++ python/day01.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def fix_expense_report(report: list) -> int:
     report_subtract_2020 = 2020 - np.array(report)
     for val in report_subtract_2020:
@@ -7,5 +8,12 @@ def fix_expense_report(report: list) -> int:
             return val * (2020 - val)
     return 0
 
-def fix_expense_report_part_two(report: list) _. int:
+
+def fix_expense_report_part_two(report: list) -> int:
+    for v1 in report:
+        for v2 in report:
+            for v3 in report:
+                if v1 + v2 + v3 == 2020:
+                    return v1 * v2 * v3
     return 0
+
--- python/day01_main.py
+++ python/day01_main.py
@@ -1,6 +1,7 @@
-from python.day01 import fix_expense_report
+from python.day01 import fix_expense_report, fix_expense_report_part_two
 
 with open("input/day01.txt") as f:
     input = f.readlines()
 input = [int(x.strip()) for x in input]
 print(fix_expense_report(input))
+print(fix_expense_report_part_two(input))
