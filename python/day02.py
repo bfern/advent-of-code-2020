3b782f2ffa9f3ea10179fd7a1321a4fb8b528c0f
Brad <bfern94@yahoo.com>
2020-12-03 20:13:26 +0000

    day 2 code

--- python/day02.py
+++ python/day02.py
@@ -1,2 +1,27 @@
 def count_valid_passwords(passwords: list) -> int:
-    return 0
+    num_valid_passwords = 0
+    for password in passwords:
+        lower_bound = get_lower_bound(password)
+        upper_bound = get_upper_bound(password)
+        letter = get_letter(password)
+        word = get_word(password)
+        num_occurrences = word.count(letter)
+        if num_occurrences >= lower_bound and num_occurrences <= upper_bound:
+            num_valid_passwords += 1
+    return num_valid_passwords
+
+
+def get_lower_bound(password: str) -> int:
+    return int(password.split("-")[0])
+
+
+def get_upper_bound(password: str) -> int:
+    return int(password.split("-")[1].split(" ")[0])
+
+
+def get_letter(password: str) -> str:
+    return password.split(":")[0][-1]
+
+
+def get_word(password: str) -> str:
+    return password.split(":")[1][1:]
