9d1d379da17d29dc7a6797b1e2b4212fd0f04326
Brad <bfern94@yahoo.com>
2020-12-04 13:44:56 +0000

    main method part 3

--- python/day03_main.py
+++ python/day03_main.py
@@ -1,6 +1,7 @@
-from python.day03 import count_number_of_trees_encountered
+from python.day03 import count_number_of_trees_encountered, count_number_of_trees_encountered_part_two
 
 with open("input/day03.txt") as f:
     input = f.readlines()
 input = [x.strip() for x in input]
 print(count_number_of_trees_encountered(input))
+print(count_number_of_trees_encountered_part_two(input))
