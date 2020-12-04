ba39d4634a17609077718f3a3c3079f8c8196e86
Brad <bfern94@yahoo.com>
2020-12-04 18:03:26 +0000

    attempt so far

--- python/day04.py
+++ python/day04.py
@@ -1,2 +1,31 @@
+import ast
+
+
 def count_valid_passports(file: str) -> int:
-    return 0
+    passports = convert_file_to_passports(file)
+    num_valid_passports = 0
+    required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    for passport in passports:
+        if passport_has_required_fields(passport, required_fields):
+            num_valid_passports += 1
+    return num_valid_passports
+
+
+def convert_file_to_passports(file: str) -> list:
+    with open(file) as f:
+        input = f.read()
+    input = input.split("\n\n")
+    passports = []
+    for val in input:
+        passport = {}
+        for v in val.strip("\n").split(" "):
+            passport[v.split(":")[0]] = v.split(":")[1]
+        passports.append(passport)
+    return passports
+
+
+def passport_has_required_fields(passport: dict, required_fields: list) -> bool:
+    for field in required_fields:
+        if field not in passport.keys():
+            return False
+    return True
