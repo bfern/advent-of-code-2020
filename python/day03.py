3ac799898e036539a5f1e329126193951dc03d80
Brad <bfern94@yahoo.com>
2020-12-04 13:39:34 +0000

    making function more extendible

--- python/day03.py
+++ python/day03.py
@@ -1,9 +1,9 @@
-def count_number_of_trees_encountered(forest: list) -> int:
+def count_number_of_trees_encountered(forest: list, right: int=3, down: int=1) -> int:
     traversing = True
     num_trees_encountered = 0
     current_loc = [0, 0]
     while traversing:
-        current_loc = go_to_next_loc(current_loc)
+        current_loc = go_to_next_loc(current_loc, right, down)
         if is_valid_location(current_loc, forest):
             if is_tree(current_loc, forest):
                 num_trees_encountered += 1
@@ -12,8 +12,8 @@ def count_number_of_trees_encountered(forest: list) -> int:
     return num_trees_encountered
 
 
-def go_to_next_loc(current_loc: list) -> list:
-    return [current_loc[0] + 3, current_loc[1] + 1]
+def go_to_next_loc(current_loc: list, right: int=3, down: int=1) -> list:
+    return [current_loc[0] + right, current_loc[1] + down]
 
 
 def is_valid_location(current_loc: list, forest: list) -> bool:
