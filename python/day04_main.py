ca029eb5565f7bfb2598a2e91834f3e6017aad9d
Brad <bfern94@yahoo.com>
2020-12-05 13:36:30 +0000

    get result for part two

--- python/day04_main.py
+++ python/day04_main.py
@@ -1,4 +1,5 @@
-from python.day04 import count_valid_passports
+from python.day04 import count_valid_passports, count_valid_passports_part_two
 import os
 
 print(count_valid_passports(os.path.join("input", "day04.txt")))
+print(count_valid_passports_part_two(os.path.join("input", "day04.txt")))
