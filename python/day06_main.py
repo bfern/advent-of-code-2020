d7bc07b008f55f6683e4c6565318d2ad569d377f
Brad <bfern94@yahoo.com>
2020-12-06 15:08:02 +0000

    day 6 part two answer

--- python/day06_main.py
+++ python/day06_main.py
@@ -1,4 +1,5 @@
-from python.day06 import sum_of_counts
+from python.day06 import sum_of_counts, sum_of_counts_part_two
 import os
 
 print(sum_of_counts(os.path.join("input", "day06.txt")))
+print(sum_of_counts_part_two(os.path.join("input", "day06.txt")))
