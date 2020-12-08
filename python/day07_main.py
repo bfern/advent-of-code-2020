142edc8f8f99bdb1ae3ba5b81df433b015364118
Brad <bfern94@yahoo.com>
2020-12-08 20:04:17 +0000

    get answer for day 7 part two

--- python/day07_main.py
+++ python/day07_main.py
@@ -1,4 +1,5 @@
-from python.day07 import get_num_bag_colours_containing_shiny_gold_bag
+from python.day07 import get_num_bag_colours_containing_shiny_gold_bag, get_number_of_bags_shiny_gold_bag_contains
 import os
 
 print(get_num_bag_colours_containing_shiny_gold_bag(os.path.join("input", "day07.txt")))
+print(get_number_of_bags_shiny_gold_bag_contains(os.path.join("input", "day07.txt")))
